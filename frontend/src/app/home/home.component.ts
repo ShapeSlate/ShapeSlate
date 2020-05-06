22a11d194edae34a6cc410ede6d6ee2357f506c4
Tyr-Lys <kaasdata@gmail.com>
2020-05-06 15:20:55 +0200

    Fixed chat in the master and resolved small merge issue that arose which caused some naming issues.

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ViewEncapsulation, OnInit} from '@angular/core';
+import * as EmojiPicker from "vanilla-emoji-picker";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit{
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit():void{
+    new EmojiPicker;
   }
 
+  
 }
